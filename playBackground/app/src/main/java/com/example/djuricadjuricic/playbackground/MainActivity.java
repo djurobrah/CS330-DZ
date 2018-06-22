ee8202fa4f179790a9b22604159c4fd4ad3b173e
Djurica Djuricic <p3ntaphr3ak@gmail.com>
2018-06-22 19:27:16 +0200

    Play music in background project added.
    Fully working.

---
+++ playBackground/app/src/main/java/com/example/djuricadjuricic/playbackground/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.djuricadjuricic.playbackground;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent svc=new Intent(this, BackgroundSoundService.class);
+        startService(svc);
+    }
+}
